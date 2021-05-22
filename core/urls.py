9d57ffb931e6dcdee95e295680b1d940e1a2ef08
Leonardo Nunes <leonardo21.nunes@gmail.com>
2021-05-21 22:51:23 -0300

    Tentando consertar media

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import index, contato, produto
 
 urlpatterns = [
     path('', index, name='index'),
     path('contato/', contato, name='contato'),  # a barra / é opcional
     path('produto/', produto, name='produto'),  # a barra / é opcional
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
